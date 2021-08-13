1e178b2a7b77ca7c4fcf7bbbed0cbc7345aeea2d
NicolasPicoG <nicolaspicog@gmail.com>
2021-08-12 19:16:39 -0500

    Actualización archivos

---
+++ js/Agente.js
@@ -0,0 +1,67 @@
+var agente = {
+    nombre: 'aspiradora'
+};
+
+
+var espacio = {
+    estado: true
+};
+
+var contadorMov;
+
+
+function moverAgente() {
+    if (document.getElementById("espacio1Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />') {
+        document.getElementById("espacio1Agente").innerHTML = '<img id="limpio" src="images/limpio.PNG" />';
+        document.getElementById("espacio2Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />';
+    }
+    limpiar();
+    prueba2();
+    contadorMov++;
+
+}
+
+function prueba() {
+    if (document.getElementById("espacio1Agente").innerHTML = '<img id="agente" src="images/limpio.PNG" />') {
+        document.getElementById("espacio2Agente").innerHTML = '<img id="limpio" src="images/limpio.PNG" />';
+        document.getElementById("espacio1Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />';
+    }
+    contadorMov++;
+}
+
+
+function limpiar() {
+    if ((document.getElementById("espacio1Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />')
+        && (document.getElementById("espacio1Basura").innerHTML = '<img id="agente" src="images/basura.PNG" />')) {
+        document.getElementById("espacio1Basura").innerHTML = '<img id="agente" src="images/limpio.PNG" />';
+        document.getElementById("espacio2Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />';
+        document.getElementById("espacio1Agente").innerHTML = '<img id="agente" src="images/limpio.PNG" />';
+    }
+}
+
+function prueba2() {
+    if ((document.getElementById("espacio2Agente").innerHTML = '<img id="agente" src="images/aspiradora.PNG" />')
+        && (document.getElementById("espacio2Basura").innerHTML = '<img id="agente" src="images/basura.PNG" />')) {
+        document.getElementById("espacio2Basura").innerHTML = '<img id="agente" src="images/limpio.PNG" />';
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
