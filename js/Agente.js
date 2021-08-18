a21a74d57d3cfd2ecd7aae5db7a66e9ecf72317c
NicolasPicoG <nicolaspicog@gmail.com>
2021-08-17 19:31:53 -0500

    Update to english

--- js/Agente.js
+++ js/Agente.js
@@ -1,7 +1,7 @@
 function reflex_agent(location, state){
-    if (state=="Sucio") return "Limpio";
-    else if (location=="A") return "Mover a la derecha";
-    else if (location=="B") return "Mover a la izquierda";
+    if (state=="Dirty") return "Clean";
+    else if (location=="A") return "Move right";
+    else if (location=="B") return "Move left";
 }
 
 function initial(states){
@@ -31,55 +31,55 @@ function test(states, counter_case, counter_operations){
 
       var action_result = reflex_agent(location, state);
       states = getDirty(states);
-      document.getElementById("log").innerHTML+="<br>".concat(counter_operations).concat(") Sector: ").concat(location).concat(" | Acción: ").concat(action_result);
+      document.getElementById("log").innerHTML+="<br>".concat(counter_operations).concat(") Position: ").concat(location).concat(" | Action: ").concat(action_result);
       counter_case = count_cases(states, counter_case);
 
-      if (action_result == "Limpio"){
-        if (location == "A") states[1] = "Limpio";
-        else if (location == "B") states[2] = "Limpio";
+      if (action_result == "Clean"){
+        if (location == "A") states[1] = "Clean";
+        else if (location == "B") states[2] = "Clean";
       }
-      else if (action_result == "Mover a la derecha") states[0] = "B";
-      else if (action_result == "Mover a la izquierda") states[0] = "A";		
+      else if (action_result == "Move right") states[0] = "B";
+      else if (action_result == "Move left") states[0] = "A";		
   time = setTimeout(function(){ test(states, counter_case, counter_operations); analyze_case(counter_case);}, 1500);
 }
 
 function count_cases(states, counter_case){
-  if((states[0] == "A") && (states[1] == "Sucio") && (states[2] == "Sucio")){
+  if((states[0] == "A") && (states[1] == "Dirty") && (states[2] == "Dirty")){
     counter_case.set("1", (counter_case.get("1") + 1));
     document.getElementById("case1_c").innerHTML=counter_case.get("1");
     document.getElementById("picture").src = "img/1_1.png";
   }
-  else if((states[0] == "B") && (states[1] == "Sucio") && (states[2] == "Sucio")){
+  else if((states[0] == "B") && (states[1] == "Dirty") && (states[2] == "Dirty")){
     counter_case.set("2", (counter_case.get("2") + 1));
     document.getElementById("case2_c").innerHTML=counter_case.get("2");
     document.getElementById("picture").src = "img/1_2.png";
   }
-  else if((states[0] == "A") && (states[1] == "Sucio") && (states[2] == "Limpio")){
+  else if((states[0] == "A") && (states[1] == "Dirty") && (states[2] == "Clean")){
     counter_case.set("3", (counter_case.get("3") + 1));
     document.getElementById("case3_c").innerHTML=counter_case.get("3");
     document.getElementById("picture").src = "img/1_3.png";
   }
-  else if((states[0] == "B") && (states[1] == "Sucio") && (states[2] == "Limpio")){
+  else if((states[0] == "B") && (states[1] == "Dirty") && (states[2] == "Clean")){
     counter_case.set("4", (counter_case.get("4") + 1));
     document.getElementById("case4_c").innerHTML=counter_case.get("4");
     document.getElementById("picture").src = "img/1_4.png";
   }
-  else if((states[0] == "A") && (states[1] == "Limpio") && (states[2] == "Sucio")){
+  else if((states[0] == "A") && (states[1] == "Clean") && (states[2] == "Dirty")){
     counter_case.set("5", (counter_case.get("5") + 1));
     document.getElementById("case5_c").innerHTML=counter_case.get("5");
     document.getElementById("picture").src = "img/1_5.png";
   }
-  else if((states[0] == "B") && (states[1] == "Limpio") && (states[2] == "Sucio")){
+  else if((states[0] == "B") && (states[1] == "Clean") && (states[2] == "Dirty")){
     counter_case.set("6", (counter_case.get("6") + 1));
     document.getElementById("case6_c").innerHTML=counter_case.get("6");
     document.getElementById("picture").src = "img/1_6.png";
   }
-  else if((states[0] == "A") && (states[1] == "Limpio") && (states[2] == "Limpio")){
+  else if((states[0] == "A") && (states[1] == "Clean") && (states[2] == "Clean")){
     counter_case.set("7", (counter_case.get("7") + 1));
     document.getElementById("case7_c").innerHTML=counter_case.get("7");
     document.getElementById("picture").src = "img/1_7.png";
   }
-  else if((states[0] == "B") && (states[1] == "Limpio") && (states[2] == "Limpio")){
+  else if((states[0] == "B") && (states[1] == "Clean") && (states[2] == "Clean")){
     counter_case.set("8", (counter_case.get("8") + 1));
     document.getElementById("case8_c").innerHTML=counter_case.get("8");
     document.getElementById("picture").src = "img/1_8.png";
@@ -91,22 +91,22 @@ function getDirty(states){
   var option = Math.floor(Math.random() * (3 - 1) + 1);
   var state_option = Math.floor(Math.random() * (3 - 1) + 1);
   if(option == 2){
-    if(states[1] == "Limpio" && states[2]=="Sucio"){
-      states[1] = "Sucio";
-      document.getElementById("log").innerHTML+="<br><b>-- Sector: ".concat("A").concat(" | Acción: ").concat("Sucio").concat("</b>");
+    if(states[1] == "Clean" && states[2]=="Dirty"){
+      states[1] = "Dirty";
+      document.getElementById("log").innerHTML+="<br><b>-- Position: ".concat("A").concat(" | Action: ").concat("Dirty").concat("</b>");
     }
-    else if(states[1] == "Sucio" && states[2]=="Limpio"){
-      states[2] = "Sucio";
-      document.getElementById("log").innerHTML+="<br><b>-- Sector: ".concat("B").concat(" | Acción: ").concat("Sucio").concat("</b>");
+    else if(states[1] == "Dirty" && states[2]=="Clean"){
+      states[2] = "Dirty";
+      document.getElementById("log").innerHTML+="<br><b>-- Position: ".concat("B").concat(" | Action: ").concat("Dirty").concat("</b>");
     }
-    else if(states[1] == "Limpio" && states[2]=="Limpio"){
+    else if(states[1] == "Clean" && states[2]=="Clean"){
       if(state_option == 1){
-        states[1] = "Sucio";
-        document.getElementById("log").innerHTML+="<br><b>-- Sector: ".concat("A").concat(" | Acción: ").concat("Sucio").concat("</b>");
+        states[1] = "Dirty";
+        document.getElementById("log").innerHTML+="<br><b>-- Position: ".concat("A").concat(" | Action: ").concat("Dirty").concat("</b>");
       }
       else if(state_option == 2){
-        states[2] = "Sucio";
-        document.getElementById("log").innerHTML+="<br><b>-- Sector: ".concat("B").concat(" | Acción: ").concat("Sucio").concat("</b>");
+        states[2] = "Dirty";
+        document.getElementById("log").innerHTML+="<br><b>-- Position: ".concat("B").concat(" | Action: ").concat("Dirty").concat("</b>");
       }
     }
     return states;
@@ -125,5 +125,5 @@ function analyze_case(counter_case){
   }
 }
 
-var states = ["A","Sucio","Sucio"];
+var states = ["A","Dirty","Dirty"];
 initial(states);
